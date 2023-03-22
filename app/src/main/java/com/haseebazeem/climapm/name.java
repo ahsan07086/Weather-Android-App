177a886beeb190ad7809647e925e54357ee02212
Nakib Ahsan <ahsannakib335@gmail.com>
2023-03-23 01:07:29 +0600

    Change

---
+++ app/src/main/java/com/haseebazeem/climapm/name.java
@@ -0,0 +1,17 @@
+package com.haseebazeem.climapm;
+
+public class name {
+    private int b;
+
+    public name(int b) {
+        this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
