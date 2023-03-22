1251461c13f606cfbbaab68855b2d57be54b1f11
Nakib Ahsan <ahsannakib335@gmail.com>
2023-03-22 23:29:06 +0600

    changed

---
+++ app/src/main/java/com/haseebazeem/climapm/test.java
@@ -0,0 +1,13 @@
+package com.haseebazeem.climapm;
+
+public class test {
+    private int marks;
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+}
---
+++ app/src/main/java/com/haseebazeem/climapm/testt.java
@@ -0,0 +1,13 @@
+package com.haseebazeem.climapm;
+
+public class testt {
+    private int roll;
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
