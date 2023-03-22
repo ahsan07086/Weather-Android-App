0e910f354b01711eeadf8a63c4c162b59e7c9ccc
Nakib Ahsan <ahsannakib335@gmail.com>
2023-03-23 00:52:55 +0600

    commmit

---
+++ app/src/main/java/com/haseebazeem/climapm/testt2.java
@@ -0,0 +1,17 @@
+package com.haseebazeem.climapm;
+
+public class testt2 {
+    private int b;
+
+    public testt2(int b) {
+        this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
